3a0eb11fc72fcf5881119eb5fefe045ac642394c
62theories <nattawatx86@gmail.com>
2021-07-23 11:41:00 -0700

    แก้วนทุกหน้า

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
-const express = require("express") 
-const fs = require('fs') 
-const path = require('path')
-const { PDFDocument, StandardFonts, rgb } = require("pdf-lib") ;
-const moment = require('moment')
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const { PDFDocument, StandardFonts, rgb } = require("pdf-lib");
+const moment = require("moment");
 
 const app = express();
 
 app.get("/", async (req, res) => {
   const pdfDoc = await PDFDocument.load(
-    fs.readFileSync(path.join(__dirname, "..","src", "assets", "test.pdf"))
-  )
-  const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
-  const page = pdfDoc.getPage(0)
-  const { width, height } = page.getSize();
-  const form = pdfDoc.getForm()
-  const pageId = 'page0'
-  const superheroField = form.createTextField(pageId)
-  superheroField.enableReadOnly()
-  superheroField.addToPage(page, { x: 0, y: 0, width, height })
-  const dateNow = moment().add(1, 'minute')
-  const expired = {
-      year: dateNow.year(),
-      month: dateNow.month(),
-      day: dateNow.date(),
-      hour: dateNow.hour(),
-      minute: dateNow.add(1, 'minute').minute()
+    fs.readFileSync(path.join(__dirname, "..", "src", "assets", "test.pdf"))
+  );
+  await pdfDoc.embedFont(StandardFonts.TimesRoman);
+  const pages = pdfDoc.getPages();
+  let jsString = "";
+  for (let pageNumber = 0; pageNumber < pages.length; pageNumber++) {
+    let page = pages[pageNumber];
+    const { width, height } = page.getSize();
+    const pageId = `page${pageNumber}`;
+    const form = pdfDoc.getForm().createTextField(pageId);
+    form.enableReadOnly();
+    form.addToPage(page, { x: 0, y: 0, width, height });
+    jsString += `this.getField('${pageId}').display = display.hidden;`;
   }
+  const dateExpired = moment().add(1, "minute");
+  const expired = {
+    year: dateExpired.year(),
+    month: dateExpired.month(),
+    day: dateExpired.date(),
+    hour: dateExpired.hour(),
+    minute: dateExpired.add(1, "minute").minute(),
+  };
   pdfDoc.addJavaScript(
     "main",
     `
@@ -34,19 +38,18 @@ app.get("/", async (req, res) => {
     if (timeNow.getTime() >= timeExpired.getTime()) {
         app.alert('expired at '+timeExpired+'');
     } else {
-        this.getField('${pageId}').display = display.hidden;
+      app.alert('will be expired at '+timeExpired+'');
+        ${jsString}
     }`
   );
-  const pdfBytes = await pdfDoc.save()
-    const pdfBuffer = Buffer.from(pdfBytes.buffer, "binary")
-    return res
-      .status(200)
-      .header("Content-disposition", "attachment; filename=qrcode.pdf")
-      .header("Content-type", "application/pdf")
-      .send(pdfBuffer)
-
+  const pdfBytes = await pdfDoc.save();
+  const pdfBuffer = Buffer.from(pdfBytes.buffer, "binary");
+  return res
+    .status(200)
+    .header("Content-disposition", "attachment; filename=testexpire.pdf")
+    .header("Content-type", "application/pdf")
+    .send(pdfBuffer);
 });
-
-app.listen(5000, () => {
-  console.log(5000);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(process.env.PORT || 5000);
 });
